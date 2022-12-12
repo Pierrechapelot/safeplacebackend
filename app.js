fff8ae3d489ebf7dfdea3d78167dd88dfde22137
Noé <lemor.noe@gmail.com>
2022-12-12 13:32:12 +0100

    modifs route settigns

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require('./models/connection');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var settingsRouter = require('./routes/settings');
+var settingsRouter = require('./routes/settings.js');
 
 var app = express();
 
